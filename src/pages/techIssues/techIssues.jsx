fea61ee5ab78f63cef8677dc94528949189ba537
newNASA <azizbekgulomovweb@gmail.com>
2026-01-15 17:46:00 +0500

    tech page added

--- src/pages/techIssues/techIssues.jsx
+++ src/pages/techIssues/techIssues.jsx
@@ -1,9 +1,11 @@
 import './techIssues.scss';
+import techGif from '../../assets/techGif.gif';
 
 const TechIssues = () => {
   return (
     <div className="tech-issues-container"> 
-      <h1>Texnik ishlar olib bo'lyapti</h1>
+      <img src={techGif} alt="" width={200} />
+      <h1>Texnik ishlar olib borilyapti</h1>
       <h3>Tez orada yakunlanadi.</h3>
     </div>
   );
