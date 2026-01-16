c02f7c9429c340a4494ac18a890d0a42623dc4a6
newNASA <azizbekgulomovweb@gmail.com>
2026-01-16 14:49:23 +0500

    fixed

--- src/pages/loader/loader.jsx
+++ src/pages/loader/loader.jsx
@@ -1,18 +1,21 @@
+import { useEffect } from 'react';
 import './loader.scss';
 import loaderGif from '../../assets/loaderGif.gif';
 
-const Loader = () => {
-    return (
-      <div className="home-loading">
-        {/* <svg class="pl" viewBox="0 0 160 160" width="160px" height="160px" xmlns="http://www.w3.org/2000/svg">
+const Loader = ({ onFinished }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (onFinished) onFinished();
+    }, 4000);
 
-          <g class="pl__ring-rotate">
-            <circle class="pl__ring-stroke" cx="80" cy="80" r="72" fill="none" stroke="hsl(223,90%,55%)" stroke-width="16" stroke-dasharray="452.39 452.39" stroke-dashoffset="452" stroke-linecap="round" transform="rotate(-45,80,80)"></circle>
-          </g>
-        </svg> */}
-        <img src={loaderGif} alt="" />
-      </div>
-    );
+    return () => clearTimeout(timer);
+  }, [onFinished]);
+
+  return (
+    <div className="home-loading">
+      <img src={`${loaderGif}?t=${new Date().getTime()}`} alt="Loading..." />
+    </div>
+  );
 };
 
 export default Loader;
